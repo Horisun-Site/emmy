1d34da587d78c3bdcdbb9efdce72802f62daf0c5
Horisun-Site <horisunola@gmil.com>
2026-03-28 16:28:44 +0100

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Details from './pages/Details'
-import Dash from './pages/Dash'
-// import Meal from './pages/Meal'
-import Info from './pages/Info'
-import Standard from './pages/Standard'
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Dash from "./pages/Dash"
+import Info from "./pages/Info";
+import Standard from "./pages/Standard"
 
 const App = () => {
   return (
@@ -16,7 +15,6 @@ const App = () => {
         <Route path='/info' element= {<Info/>}/>
         <Route path='/dash' element= {<Dash/>}/>
         <Route path='/stance' element={<Standard/>}/>
-        {/* <Route path='/meal' element={<Meal/>}/> */}
       </Routes>
     </Router>
   )
